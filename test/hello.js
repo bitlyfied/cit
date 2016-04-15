0556af5c1040e01c119bfa923beb9fbd576e3137
Valentino Aluigi <valentino.aluigi@gmail.com>
2016-04-15 07:37:43 +0700

    Add basic mocha unit test

---
+++ test/hello.js
@@ -0,0 +1,11 @@
+var assert = require("chai").assert
+
+describe("Thing", function() {
+  it("works", function() {
+    assert.equal(true, true)
+  })
+
+  it("fails", function() {
+    assert.equal(true, false)
+  })
+})
